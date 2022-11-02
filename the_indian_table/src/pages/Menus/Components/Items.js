5db444bb6da14e23d6361f68f8f4d88937ccaf56
Pukhraj <pukhrajkhinda@gmail.com>
2022-11-02 13:27:50 +0530

    menu fix

--- the_indian_table/src/pages/Menus/Components/Items.js
+++ the_indian_table/src/pages/Menus/Components/Items.js
@@ -1,6 +1,6 @@
 function Items({ name, ingred, price }) {
   return (
-    <div className="flex justify-between  bg-slate-600 text-white p-2 rounded-lg drop-shadow-2xl m-2">
+    <div className="flex justify-between  bg-slate-600 text-white p-2 rounded-lg  m-2">
       <div className="">
         <h1
           style={{ textShadow: "0px 2px 2px black" }}
@@ -12,9 +12,14 @@ function Items({ name, ingred, price }) {
           {ingred}
         </p>
       </div>
-      <div>
+      <div
+        style={{
+          translate: "40% -35%",
+        }}
+        className="bg-yellow-600 h-12 w-12 rounded-full flex justify-center items-center"
+      >
         <p
-          className="bg-yellow-600 p-1 rounded-2xl drop-shadow-2xl"
+          className=" p-1 text-sm rounded-2xl "
           style={{ textShadow: "0px 2px 2px black" }}
         >
           {price}
