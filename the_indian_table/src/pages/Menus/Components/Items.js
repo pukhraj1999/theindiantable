c445689655a61b54558b0b131e20bbfa769fb3b4
Pukhraj <pukhrajkhinda@gmail.com>
2022-11-05 15:43:30 +0530

    menu items heading size fix

--- the_indian_table/src/pages/Menus/Components/Items.js
+++ the_indian_table/src/pages/Menus/Components/Items.js
@@ -2,7 +2,7 @@ function Items({ name, ingred, price, extra }) {
   return (
     <div className="flex justify-between  m-4">
       <div className="">
-        <h1 className="text-slate-700 font-bold text-2xl">{name}</h1>
+        <h1 className="text-slate-700 font-bold text-xl">{name}</h1>
         <p className="">{ingred}</p>
         <p className="text-red-400">{extra}</p>
       </div>
